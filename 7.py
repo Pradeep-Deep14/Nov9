c4b2fd77e4954866446fce69ed49b5528bb97ff6
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-09 19:50:32 +0530

    first commit

---
+++ 7.py
@@ -0,0 +1,9 @@
+s=set()
+s.update('hello','how','are','you?')
+print(len(s))
+
+#10
+#The set `s` contains 10 distinct characters: 
+# 'h', 'e', 'l', 'o', 'w', 'a', 'r', 'y', 'o', and '?'. 
+# This count represents the total number of unique characters from the combined strings
+# 'hello', 'how', 'are', and 'you?
